a128cce24744ec28b3204978b35b3cdf684baa14
jesslynamanda13 <jesslynmulyawan@gmail.com>
2021-07-09 09:46:00 +0700

    finalproject

---
+++ intro.js
@@ -0,0 +1,4 @@
+alert("Hello!");
+let name = prompt("What's your name? 🧐");
+alert("Hello " + name + "! Welcome to my page <3");
+console.log("Hi kakak2 from summercamp! It's really nice meeting all of youu hehe. Anyways, I hope we will have a great time together. Stay safe yall")
